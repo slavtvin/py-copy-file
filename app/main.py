3dddc1636485ae296bcb3a04fb1a1b60811b8031
Slavtvin <koffman06@gmail.com>
2026-09-15 18:49:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    first_filename = command.split(" ")[1]
+    last_filename = command.split(" ")[2]
+    if first_filename == last_filename:
+        return
+    with (open(first_filename, "r") as file_read,
+          open(last_filename, "w") as file_write):
+        file_write.write(file_read.read())
